6f509c3b302fb2388d8b048eb45b213285f3fe55
Eliu Florez <eflorez@socialh4ck.com>
2014-07-12 18:22:02 -0430

    Twitter OAuth

---
+++ src/Socialh4ck/Twitter/Facades/Twitter.php
@@ -0,0 +1,14 @@
+<?php namespace Socialh4ck\Twitter\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Twitter extends Facade {
+
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor() { return 'twitter'; }
+
+}
---
+++ src/Socialh4ck/Twitter/TwitterServiceProvider.php
@@ -0,0 +1,49 @@
+<?php namespace Socialh4ck\Twitter;
+
+use Illuminate\Support\ServiceProvider;
+
+class TwitterServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Boot the service provider.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('socialh4ck/twitter');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+
+		$this->app['twitter'] = $this->app->share(function($app)
+		{
+			return new Twitter();
+		});
+
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
